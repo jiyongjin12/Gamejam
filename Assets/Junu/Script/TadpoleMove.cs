2502c2d8b4731ac9b3227a93e2c6aaa92787c4bf
creaMelon <junu0509_love@naver.com>
2023-07-22 22:24:39 +0900

    Add tadpole

---
+++ Assets/Junu/Script/TadpoleMove.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TadpoleMove : MonoBehaviour
+{
+    public Sprite[] tadPole_Row_Sprite;
+    public Sprite[] tadPole_Mid_Sprite;
+    public Sprite[] tadPole_High_Sprite;
+
+    SpriteRenderer sr;
+
+    [SerializeField]
+    int level = 1;
+
+    public bool isSprite = true;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            if(isSprite)
+            {
+                sr.sprite = tadPole_Row_Sprite[1];
+                isSprite = false;
+            }
+            else if(!isSprite)
+            {
+                sr.sprite = tadPole_Row_Sprite[0];
+                isSprite = true;
+            }
+        }
+    }
+}
